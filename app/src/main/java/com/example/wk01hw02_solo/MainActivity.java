2edd743e3f5f717c12a1c9b1d37d28a2a0286597
katgrose <kathryngrose@gmail.com>
2021-09-03 19:36:13 -0700

    Login page implemented, no tests

--- app/src/main/java/com/example/wk01hw02_solo/MainActivity.java
+++ app/src/main/java/com/example/wk01hw02_solo/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.wk01hw02_solo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,50 +20,89 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button loginButton;
+    private EditText username;
+    private EditText password;
+
     private TextView textViewResult;
+    private TextView welcomeMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        textViewResult = findViewById(R.id.text_view_result);
+        loginButton = findViewById(R.id.login_button);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://jsonplaceholder.typicode.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
-
-        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
-
-        call.enqueue(new Callback<List<Post>>() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-
-                if (!response.isSuccessful()) {
-                    textViewResult.setText("Code: " + response.code());
-                    return;
-                }
-
-                List<Post> posts = response.body();
-
-                for (Post post: posts) {
-                    String content = "";
-                    content += "ID: " + post.getId() + "\n";
-                    content += "User ID: " + post.getUserId() + "\n";
-                    content += "Title: " + post.getTitle() + "\n";
-                    content += "Text: " + post.getText() + "\n\n";
-
-                    textViewResult.append(content);
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Post>> call, Throwable t) {
-                textViewResult.setText(t.getMessage());
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                verifyAccount(user, pass);
             }
         });
+    }
+
+    public void verifyAccount(String user, String pass) {
+        List<Users> usersList = Users.getUsersList();
+
+        Users activeUser = Users.getUserbyUsername(user, usersList);
+
+        if (activeUser == null) {
+            Toast.makeText(MainActivity.this, "Bad Username", Toast.LENGTH_SHORT).show();
+        }
+        else if (!activeUser.getPassword().equals(pass)) {
+            Toast.makeText(MainActivity.this, "Bad Password", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            setContentView(R.layout.activity_main);
+            textViewResult = findViewById(R.id.text_view_result);
+
+            textViewResult.append("Welcome " + activeUser.getUsername()
+                    + " [ User ID: " + activeUser.getUserID() + " ]!\n\n" );
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://jsonplaceholder.typicode.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+            Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+
+            call.enqueue(new Callback<List<Post>>() {
+                @Override
+                public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                    if (!response.isSuccessful()) {
+                        textViewResult.setText("Code: " + response.code());
+                        return;
+                    }
+
+                    List<Post> posts = response.body();
+
+                    for (Post post : posts) {
+                        if (post.getUserId() == activeUser.getUserID()) {
+                            String content = "";
+                            content += "ID: " + post.getId() + "\n";
+                            content += "User ID: " + post.getUserId() + "\n";
+                            content += "Title: " + post.getTitle() + "\n";
+                            content += "Text: " + post.getText() + "\n\n";
+
+                            textViewResult.append(content);
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<Post>> call, Throwable t) {
+                    textViewResult.setText(t.getMessage());
+                }
+
+            });
+        }
     }
 }
---
+++ app/src/main/java/com/example/wk01hw02_solo/Users.java
@@ -0,0 +1,76 @@
+package com.example.wk01hw02_solo;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Users {
+    private String username;
+    private String password;
+    private int userID;
+
+    private static final List<Users> usersList = new ArrayList<>();
+
+    private static void dummy_accounts() {
+        usersList.add(new Users("admin", "admin_pass"));
+        usersList.add(new Users("user1", "password1"));
+        usersList.add(new Users("user2", "password2"));
+        usersList.add(new Users("user3", "password3"));
+        usersList.add(new Users("user4", "password4"));
+        usersList.add(new Users("user5", "password5"));
+        usersList.add(new Users("user6", "password6"));
+        usersList.add(new Users("user7", "password7"));
+        usersList.add(new Users("user8", "password8"));
+        usersList.add(new Users("user9", "password9"));
+    }
+
+    public static List<Users> getUsersList() {
+        if(usersList.isEmpty()) {
+            dummy_accounts();
+        }
+
+        return usersList;
+    }
+
+    private int id_iter = 1;
+    public Users(String username, String password) {
+        this.userID = id_iter++;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public static Users getUserbyUsername(String username, List<Users> usersList) {
+        for(Users user: usersList) {
+            if(user.getUsername().equals(username)) {
+                return user;
+            }
+            Log.i("getUser", username + " " + user.getUsername());
+        }
+        return null;
+    }
+}
